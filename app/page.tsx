8ef7e431cef17a21644aacbe7480fb2b1d35245d
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-06-06 16:18:45 +0200

    remove div

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,9 @@ import Form from './components/Form';
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Hello world</h1>
-        <Form />
-      </div>
+    <main className='bg-slate-800 h-[100vh] text-white'>
+      <h1>Hello world</h1>
+      <Form />
     </main>
   );
 }
