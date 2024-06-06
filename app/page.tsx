b4e1795d1d3e8f027cbfaabbd0c155e6f85889b6
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-06-06 16:53:23 +0200

    add camera logo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Form from './components/Form';
 export default function Home() {
   return (
     <main className='bg-slate-800 h-[100vh] text-white'>
-      <h1 className='text-center py-10 text-4xl'>Scan Me!</h1>
+      <h1 className='text-center py-10 text-4xl'>Scan Me! 📸</h1>
       <Form />
     </main>
   );
