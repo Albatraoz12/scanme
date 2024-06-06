70ce78b78565cbb76313b594da0843a229b9e6a7
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-06-06 16:26:07 +0200

    add styling for form and input

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Form = () => {
   return (
-    <section className=' px-4'>
-      <form action='' className=' flex justify-between border'>
-        <div className='flex flex-col'>
+    <section className='flex justify-center items-center px-4 mx-10'>
+      <form className='flex flex-col justify-center items-center border w-full text-center gap-3 py-5'>
+        <div className='input-container'>
           <label htmlFor='test-1'>Test 1</label>
-          <input type='text' id='test-1' />
+          <input type='text' id='test-1' className='border rounded' />
         </div>
-        <div className='flex flex-col'>
+        <div className='input-container'>
           <label htmlFor='test-2'>Test 2</label>
           <input type='text' id='test-2' />
         </div>
-        <div className='flex flex-col'>
+        <div className='input-container'>
           <label htmlFor='test-3'>Test 3</label>
           <input type='text' id='test-3' />
         </div>
