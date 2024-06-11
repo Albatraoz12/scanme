f2d52e12a8872df25ec304a0891c95dfbb15de24
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2024-06-11 20:39:52 +0200

    create state to store imgIndo and test convertor api

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,14 +1,27 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import convertor from '../lib/converter';
+import { IFormData } from '../Interfaces/IFormData';
 
 const Form = () => {
+  const [formData, setFormData] = useState<IFormData>({
+    itemName: '',
+    articleNum: '',
+    floorNum: '',
+  });
+
+  const handleClick = async () => {
+    // Static for the time being. will be imported with a state to function
+    const imgInfo = await convertor();
+    console.log(imgInfo);
+  };
+
   return (
     <section className='flex justify-center items-center px-4 mx-10'>
       <form className='flex flex-col justify-center items-center w-full text-center gap-3 py-5'>
         <div className='input-container'>
-          <label htmlFor='name'>Name</label>
-          <input type='text' id='num' className='border rounded' />
+          <label htmlFor='itemName'>Name</label>
+          <input type='text' id='itemName' className='border rounded' />
         </div>
         <div className='input-container'>
           <label htmlFor='articleNum'>Article number</label>
@@ -19,17 +32,17 @@ const Form = () => {
           <input type='text' id='floorNum' />
         </div>
         <div>
-          <button className='btn bg-green-600 hover:bg-green-900'>
+          <button className='btn bg-green-600 hover:bg-green-900' type='submit'>
             Submit
           </button>
         </div>
         <div>
           <button
             className='btn bg-green-600 hover:bg-green-900'
-            onClick={() => convertor()}
+            onClick={() => handleClick()}
             type='button'
           >
-            test
+            Test
           </button>
         </div>
       </form>
